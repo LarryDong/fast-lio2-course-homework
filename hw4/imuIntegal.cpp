6955d0115db72fdd24cd3a4e986bab6824ddd86a
larrydong <jfjxxdy2007@126.com>
2023-09-21 15:15:50 +0800

    hw4 answer submit

--- hw4/imuIntegal.cpp
+++ hw4/imuIntegal.cpp
@@ -14,10 +14,28 @@ bool first_imu = true;
 void imuCallback(const sensor_msgs::Imu::ConstPtr &msg_in)
 {
   //------------- 开始你的代码 --------------------//
+  double dt = 1.0/200;    // IMU frequency is 200Hz;
+  #define G 9.8
+  ROS_INFO("Receive IMU");
+  double ax, ay, az;
+  double wx, wy, wz;
+  ax = msg_in->linear_acceleration.x*G;         // livox IMU is normalized.
+  ay = msg_in->linear_acceleration.y*G;
+  az = msg_in->linear_acceleration.z*G;
 
+  wx = msg_in->angular_velocity.x;
+  wy = msg_in->angular_velocity.y;
+  wz = msg_in->angular_velocity.z;
+ 
+  ROS_INFO_STREAM("IMU data: acc:[ " << ax << ", " << ay << ", " << az << "], gyro: [" << wx << ", " << wy << ", " << wz << "]. ");
 
-
-
+  Sophus::Vector3d w(wx, wy, wz);
+  Sophus::Vector3d a(ax, ay, az);
+  
+  pos = pos + vel*dt;
+  vel = vel + rot*((a-ba)+grav)*dt;
+  rot = rot * Sophus::SO3::exp((w-bg)*dt);
+  ROS_INFO_STREAM("IMU update: R(t+dt): " << rot <<" \n  v: " << vel.transpose() <<" \n  p: " << pos.transpose());
 
   //------------- 结束你的代码 --------------------//
 }
@@ -27,7 +45,7 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "imu_integal");
   ros::NodeHandle nh;
 
-  ros::Subscriber sub = nh.subscribe("/imu/data", 10000, imuCallback);
+  ros::Subscriber sub = nh.subscribe("/livox/imu", 10000, imuCallback);
   ros::spin();
 
   return 0;
--- hw4/pointCouldFiltering.cpp
+++ hw4/pointCouldFiltering.cpp
@@ -11,11 +11,27 @@ ros::Publisher downsampled_pub;
 void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
     // --------- 开始你的代码	---------------//
+    pcl::PointCloud<pcl::PointXYZ> pc, pc_out, pc_out2;
+    pcl::fromROSMsg(*cloud_msg, pc);
+    ROS_INFO_STREAM("Input cloud size: " << pc.points.size());
 
+    pcl::VoxelGrid<pcl::PointXYZ> dsFilter;
+    dsFilter.setLeafSize(0.1,0.1,0.1);
+    dsFilter.setInputCloud(pc.makeShared());
+    dsFilter.filter(pc_out);
+    ROS_INFO_STREAM("Downsize filter: " << pc_out.points.size());
 
+    for(auto p : pc_out.points){
+        if(abs(p.x) < 3)
+          pc_out2.points.push_back(p);
+    }
+    ROS_INFO_STREAM("Remove x>3 : " << pc_out2.points.size());
 
-
-    
+    // publish 
+    sensor_msgs::PointCloud2 pc_msg;
+    pcl::toROSMsg(pc_out2, pc_msg);
+    pc_msg.header = cloud_msg->header;
+    downsampled_pub.publish(pc_msg);
     // --------- 结束你的代码	---------------//
 }
 
@@ -24,7 +40,7 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "point_cloud_processing");
   ros::NodeHandle nh;
 
-  ros::Subscriber sub = nh.subscribe("/rslidar_points", 1, pointCloudCallback);
+  ros::Subscriber sub = nh.subscribe("/livox/lidar", 1, pointCloudCallback);
   downsampled_pub = nh.advertise<sensor_msgs::PointCloud2>("/downsampled/cloud", 1);
 
   ros::spin();
